8560cb12cac96273390f872ec6f66bde84d0b062
albertbn <albertbn>
2016-02-15 16:04:26 +0200

    cloned repo, added several things, wrote remarks - go on with connecting the 2 client controls - node tree and chart

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 //Lets define a port we want to listen to
-const PORT=8082; 
+const PORT=8082;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
@@ -25,7 +25,7 @@ io.on('connection', function(socket){
 });
 
 var tree = [
-    
+
     {countryID:1, countryName:'USA', cityID:1, cityName:'NY', boxID:777, boxStatus:1},
     {countryID:1, countryName:'USA', cityID:1, cityName:'NY'},
     {countryID:1, countryName:'USA', cityID:1, cityName:'NY'},
@@ -37,7 +37,7 @@ var data = [
     { boxID:777,data_actual:[.6, .4, 1.2] },
     { boxID:777,data_actual:[.6, .4, 1.2] },
     { boxID:777,data_actual:[.6, .4, 1.2] }
-]
+];
 
 function simulate_and_return_tree(){
     //TODO - do random row number manipulation (ex. ros 2,4)
@@ -48,14 +48,15 @@ function simulate_and_return_tree(){
 
 function random_event_emitter() {
 
-//TODO - enter the real JSON
+  //TODO - enter the real JSON
 
-    setInterval( function(){  
-        io.emit("jimi", "jimi from server") } , 
-        // io.emit("tree",  model) } , 
-        // io.emit("data",  data) } , 
-        10000 /*TODO random(5000, 30000)*/  );
-    // io.emit(/emits to ALL sockets/);
+  setInterval( function(){
+    io.emit("jimi", "jimi from server");
+  },
+               // io.emit("tree",  model) } ,
+               // io.emit("data",  data) } ,
+               10000 /*TODO random(5000, 30000)*/  );
+  // io.emit(/emits to ALL sockets/);
 }
 
 function random(min, max){
